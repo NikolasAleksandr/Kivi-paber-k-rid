2de1da6ca54f3b996d2f50714c9da9f6ad024493
Kolaleila <aleksandr.nikolas1@gmail.com>
2023-12-14 02:22:08 +0300

    .

--- Kivi-paber-käärid/Kivi_paber_käärid.py
+++ Kivi-paber-käärid/Kivi_paber_käärid.py
@@ -1,57 +1,68 @@
 ﻿import random
 import time
 
-def mängi_raund(mängija1, mängija2):
-    märgid = ["kivi", "käärid", "paber"]
+def play_round(player1, player2):
+    gestures = ["kivi", "käärid", "paber"]
 
     print("kivi... käärid... paber... üks... kaks... kolm!")
 
-    märk1 = input(f"{mängija1}, vali märk (kivi/käärid/paber): ").lower()
-    while märk1 not in märgid:
-        print("Viga: Vali kehtiv märk!")
-        märk1 = input(f"{mängija1}, vali märk (kivi/käärid/paber): ").lower()
+    gesture1 = input(f"{player1}, loe 'kolm' ja näita žesti (kivi/käärid/paber): ").lower()
+    while gesture1 not in gestures:
+        print("Viga: Vali kehtiv žest!")
+        gesture1 = input(f"{player1}, loe 'kolm' ja näita žesti (kivi/käärid/paber): ").lower()
 
-    märk2 = input(f"{mängija2}, vali märk (kivi/käärid/paber): ").lower()
-    while märk2 not in märgid:
-        print("Viga: Vali kehtiv märk!")
-        märk2 = input(f"{mängija2}, vali märk (kivi/käärid/paber): ").lower()
+    gesture2 = input(f"{player2}, loe 'kolm' ja näita žesti (kivi/käärid/paber): ").lower()
+    while gesture2 not in gestures:
+        print("Viga: Vali kehtiv žest!")
+        gesture2 = input(f"{player2}, loe 'kolm' ja näita žesti (kivi/käärid/paber): ").lower()
 
-    print(f"{mängija1} näitas {märk1}")
-    print(f"{mängija2} näitas {märk2}")
+    print(f"{player1} näitas {gesture1}")
+    print(f"{player2} näitas {gesture2}")
 
-    if märk1 == märk2:
-        return "Viik"
-    elif (märk1 == "kivi" and märk2 == "käärid") or \
-         (märk1 == "käärid" and märk2 == "paber") or \
-         (märk1 == "paber" and märk2 == "kivi"):
-        return f"{mängija1} võidab!"
+    if (gesture1 == "paber" and gesture2 == "kivi"):
+        return f"{player1} võidab! Paber mähib kivi ümber."
+    elif (gesture1 == "kivi" and gesture2 == "käärid"):
+        return f"{player1} võidab! Kivi tuhmib või purustab käärid."
+    elif (gesture1 == "käärid" and gesture2 == "paber"):
+        return f"{player1} võidab! Käärid lõikavad paberit."
+    elif (gesture2 == "paber" and gesture1 == "kivi"):
+        return f"{player2} võidab! Paber mähib kivi ümber."
+    elif (gesture2 == "kivi" and gesture1 == "käärid"):
+        return f"{player2} võidab! Kivi tuhmib või purustab käärid."
+    elif (gesture2 == "käärid" and gesture1 == "paber"):
+        return f"{player2} võidab! Käärid lõikavad paberit."
     else:
-        return f"{mängija2} võidab!"
-
+        return "Viik"
+    
 def main():
-    mängija1 = input("Sisesta esimese mängija nimi: ")
-    mängija2 = input("Sisesta teise mängija nimi (või sisesta 'robot' jaoks): ")
+    player1 = input("Sisesta esimese mängija nimi: ")
+    player2 = input("Sisesta teise mängija nimi: ")
 
-    skoor = {mängija1: 0, mängija2: 0}
-    raundid = 3
+    scores = {player1: 0, player2: 0}
+    rounds = 3
 
-    for _ in range(raundid):
-        tulemus = mängi_raund(mängija1, mängija2)
-        print(tulemus)
+    for _ in range(rounds):
+        result = play_round(player1, player2)
+        print(result)
 
-        if "võidab" in tulemus:
-            võitja = tulemus.split()[0]
-            skoor[võitja] += 1
+        if "võidab" in result:
+            winner = result.split()[0]
+            scores[winner] += 1
 
-        print(f"Aktuaalne skoor: {mängija1} {skoor[mängija1]} - {skoor[mängija2]} {mängija2}")
+        print(f"Aktuaalne skoor: {player1} {scores[player1]} - {scores[player2]} {player2}")
         time.sleep(1)
 
     print("\nLõplik skoor:")
-    print(f"{mängija1}: {skoor[mängija1]} punkti")
-    print(f"{mängija2}: {skoor[mängija2]} punkti")
+    print(f"{player1}: {scores[player1]} punkti")
+    print(f"{player2}: {scores[player2]} punkti")
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
 
 
 
