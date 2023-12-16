abf9470a4d47a549d5c50594e0cec18528911fd5
Kolaleila <aleksandr.nikolas1@gmail.com>
2023-12-16 04:36:23 +0300

    changed

--- Kivi-paber-käärid/Kivi_paber_käärid.py
+++ Kivi-paber-käärid/Kivi_paber_käärid.py
@@ -1,10 +1,17 @@
-﻿import random
-import time
+﻿from random import *
 
-def play_round(player1, player2):
-    žest = ["kivi", "käärid", "paber"]
+žest = ["kivi", "käärid", "paber"]
 
-    print("kivi... käärid... paber... üks... kaks... kolm!")
+player1 = input("Sisesta esimese mängija nimi: ")
+player2 = input("Sisesta teise mängija nimi: ")
+
+print("kivi... käärid... paber... üks... kaks... kolm!")
+
+scores = {player1: 0, player2: 0}
+rounds = 3
+
+for _ in range(rounds):
+    print(f"\nRound {_ + 1} algab!")
 
     žest1 = input(f"{player1}, loe 'kolm' ja näita žesti (kivi/käärid/paber): ").lower()
     while žest1 not in žest:
@@ -19,45 +26,23 @@ def play_round(player1, player2):
     print(f"{player1} näitas {žest1}")
     print(f"{player2} näitas {žest2}")
 
-    if (žest1 == "paber" and žest2 == "kivi"):
-        return f"{player1} võidab! Paber mähib kivi ümber."
-    elif (žest1 == "kivi" and žest2 == "käärid"):
-        return f"{player1} võidab! Kivi tuhmib või purustab käärid."
-    elif (žest1 == "käärid" and žest2 == "paber"):
-        return f"{player1} võidab! Käärid lõikavad paberit."
-    elif (žest2 == "paber" and žest1 == "kivi"):
-        return f"{player2} võidab! Paber mähib kivi ümber."
-    elif (žest2 == "kivi" and žest1 == "käärid"):
-        return f"{player2} võidab! Kivi tuhmib või purustab käärid."
-    elif (žest2 == "käärid" and žest1 == "paber"):
-        return f"{player2} võidab! Käärid lõikavad paberit."
+    if (žest1 == "paber" and žest2 == "kivi") or (žest1 == "kivi" and žest2 == "käärid") or (žest1 == "käärid" and žest2 == "paber"):
+        print(f"{player1} võidab! {žest1} mähib {žest2} ümber.")
+        scores[player1] += 1
+    elif (žest2 == "paber" and žest1 == "kivi") or (žest2 == "kivi" and žest1 == "käärid") or (žest2 == "käärid" and žest1 == "paber"):
+        print(f"{player2} võidab! {žest2} mähib {žest1} ümber.")
+        scores[player2] += 1
     else:
-        return "Viik"
-    
-def main():
-    player1 = input("Sisesta esimese mängija nimi: ")
-    player2 = input("Sisesta teise mängija nimi: ")
+        print("Viik! Mäng mängitakse uuesti.")
 
-    scores = {player1: 0, player2: 0}
-    rounds = 3
+    print(f"Aktuaalne skoor: {player1} {scores[player1]} - {scores[player2]} {player2}")
 
-    for _ in range(rounds):
-        result = play_round(player1, player2)
-        print(result)
+print("\nLõplik skoor:")
+print(f"{player1}: {scores[player1]} punkti")
+print(f"{player2}: {scores[player2]} punkti")
 
-        if "võidab" in result:
-            winner = result.split()[0]
-            scores[winner] += 1
 
-        print(f"Aktuaalne skoor: {player1} {scores[player1]} - {scores[player2]} {player2}")
-        time.sleep(1)
 
-    print("\nLõplik skoor:")
-    print(f"{player1}: {scores[player1]} punkti")
-    print(f"{player2}: {scores[player2]} punkti")
-
-if __name__ == "__main__":
-    main()
 
 
 
